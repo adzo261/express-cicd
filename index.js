967a80d001e0c483e523f0fbf827bb74d4508546
Aditya Zope <adityazope261@gmail.com>
2019-04-22 22:23:29 +0530

    Added home page

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express")
+const routes = require("./routes/route")
+const path = require("path")
+const exphbs = require("express-handlebars")
+
 const app = express()
 
+app.use(express.static(path.join(__dirname, "public")))
+app.use("/", routes)
+app.set("views", path.join(__dirname, "views"))
+app.engine("hbs", exphbs({ extname: ".hbs", defaultLayout: "base" }))
+app.set("view engine", "hbs")
+
 const port = process.env.PORT || 3000
-
-app.get("/", (req, res) => res.send("Hello World!"))
-
 const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
